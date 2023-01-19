3c7c7dd8d8f024b189b4ca0ce2cc904e9b4a6697
Mizrobbek <baqoyevmizrob@gmail.com>
2023-01-19 14:51:29 +0500

    add btn menu

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
-var elTheme = document.getElementById('theme');
-var elBody = document.getElementById('body');
+var elTheme = document.getElementById("theme");
+var elBody = document.getElementById("body");
 
-elTheme.addEventListener('change', function () {
+elTheme.addEventListener("change", function () {
   if (elTheme.checked === true) {
-    elBody.className = 'dark';
+    elBody.className = "dark";
   } else {
-    elBody.className = '';
+    elBody.className = "";
   }
+});
+
+var elButton = document.querySelector(".header__menu");
+var elMenu = document.querySelector(".menu");
+
+elButton.addEventListener("click", function (evt) {
+  elMenu.classList.toggle("open-menu");
+  elButton.classList.toggle("open-btn");
 });
